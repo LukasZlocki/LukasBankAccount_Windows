ea79bccb82d1d4f4dfa032fb09ef51f20552c363
lzlocki <lzlocki@poczta.fm>
2021-01-23 14:56:45 +0100

    Add driver for program

---
+++ windowaccount_driver.py
@@ -0,0 +1,21 @@
+# Banking task by Lukasz Zlocki / 76103
+# tkinter base Windows version
+# zlocki_76103@cloud.wsb.wroclaw.pl
+
+import mainWindow_gui as win
+import tkinter as tk
+
+
+def main():
+
+    root = tk.Tk()
+    app = win.MainWindowGUI(root)
+    root.mainloop()
+
+
+
+
+
+# Call main function
+if __name__ == "__main__":
+    main()  
